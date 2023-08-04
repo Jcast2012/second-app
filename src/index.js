70f7dafad5334e5a1ba22da0566bacc6b20fcfd3
jay <jaycast2012@DESKTOP-BGBL0U0.localdomain>
2023-08-04 14:47:38 -0400

    second-app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+ import React from 'react';
+import fruit from './fruit';
+import { choice } from './helper';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+ import App from './App';
+// import reportWebVitals from './reportWebVitals';
+ import fruits from './foods';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ let fruit = choice(fruits);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ console.log ("i'd like one ${fruit}, please!");
+ console.log('here you go: ${fruit}');
+ console.log('Delicous! May I have another? ');
+
+ let remaining = remove(fruit, fruits);
+
+ console.log ('Im sorry, we are currently ou., we have ${remaining.length} left.' );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
